ffc20f600b433885bcfbeac7eb53a61335567eeb
davshikhar <shikharyadav900@gmail.com>
2026-03-16 19:42:44 +0530

    Added Prisma to the project

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -3,23 +3,23 @@ import express from "express";
 import jwt from "jsonwebtoken";
 import {z} from  "zod";
 import { middleware } from "./middleware";
+import {signupSchema, siginSchema, createRoomSchema} from "@repo/common/types";
 
 const app = express();
 const port = 3001;
 
 console.log("JWT_SECRET", process.env.JWT_SECRET);
 
-const schema = z.object({
-    name:z.string().max(20,"Name should be under 20 characters"),
-    email:z.email("Email is not valid"),
-    password:z.string().max(20, "Password should be under 20 characters")
-});
-
-const siginSchema = schema.pick({email:true,password:true});
+// const schema = z.object({
+//     name:z.string().max(20,"Name should be under 20 characters"),
+//     email:z.email("Email is not valid"),
+//     password:z.string().max(20, "Password should be under 20 characters")
+// });
+// const siginSchema = schema.pick({email:true,password:true});
 
 app.post("/signup",(req,res)=>{
-    const safe = {name:req.body.name, email:req.body.email, password:req.body.password};
-    const User = schema.safeParse(safe);
+    const safe = {username:req.body.username, password:req.body.password, name:req.body.name};
+    const User = signupSchema.safeParse(safe);
     if(!User.success){
         return res.json({
             message:"Invalid data"
@@ -33,7 +33,7 @@ app.post("/signup",(req,res)=>{
 })
 
 app.post("/signin",(req,res)=>{
-    const login = {email:req.body.email, password:req.body.password};
+    const login = {username:req.body.username, password:req.body.password};
     const loginData = siginSchema.safeParse(login);
     if(!loginData.success){
         return res.json({
@@ -42,10 +42,10 @@ app.post("/signin",(req,res)=>{
     }
     else{
         const passwordMatch = false;
-        const response = {"name":"shikhar"};
+        const response = {"username":"shikhar"};
         if(passwordMatch){
             const token = jwt.sign(
-                {name:response.name},process.env.JWT_SECRET as string
+                {username:response.username},process.env.JWT_SECRET as string
             );
             res.json({token:token});
             return;
@@ -60,6 +60,17 @@ app.post("/signin",(req,res)=>{
 
 app.post("/room",middleware,(req,res)=>{
     //db call over here to create a room
+    const data = createRoomSchema.safeParse(req.body);
+    if(!data.success){
+        res.json({
+            message:"Invalid data",
+        })
+        return;
+    }
+    res.json({
+        roomId:"12345",
+        message:"Room created successfully"
+    });
 })
 
 app.listen(port);
